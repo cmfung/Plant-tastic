1430a25a049cda2ebdb8817dd922f69f363774d5
Connor Fung <cmfung23@gmail.com>
2022-11-16 12:58:54 -0800

    pre flip testing commit

--- plantastic/src/Plant.js
+++ plantastic/src/Plant.js
@@ -7,25 +7,27 @@ import {
 function PlantCard({ plant }) {
   return (
     <Box flexGrow='1' alignSelf='center' align='center'>
-      <Image
-        src='./plantbackground.jpeg'
-      />
-      <Box borderWidth='1px' borderRadius='lg' maxW='500px' align='left'>
-        <Box fontWeight='bold' textTransform='uppercase'>{plant.common[1] ? plant.common[1] : plant.common[0]}</Box>
-        <Box fontWeight='bold' textTransform='uppercase'>Latin Name:</Box>
-        <Box pl='15px'>{plant.latin}</Box>
-        <Box fontWeight='bold' textTransform='uppercase'>Family:</Box>
-        <Box pl='15px'>{plant.family}</Box>
-        <Box fontWeight='bold' textTransform='uppercase'>Category:</Box>
-        <Box pl='15px'>{plant.category}</Box>
-        <Box fontWeight='bold' textTransform='uppercase'>Origin:</Box>
-        <Box pl='15px'>{plant.origin}</Box>
-        <Box fontWeight='bold' textTransform='uppercase'>Temperature Range:</Box>
-        <Box pl='15px'>{plant.tempmin.fahrenheit}-{plant.tempmax.fahrenheit} &#8457;</Box>
-        <Box fontWeight='bold' textTransform='uppercase'>Ideal Light:</Box>
-        <Box pl='15px'>{plant.ideallight}</Box>
-        <Box fontWeight='bold' textTransform='uppercase'>Watering Instructions:</Box>
-        <Box pl='15px'>{plant.watering}</Box>
+      <Box borderWidth='1px' maxW='500px' borderRadius='3%'>
+        <Image
+          src='https://silverbirchgardens.co.uk/wp-content/uploads/2021/08/tropical.jpg'
+          />
+        <Box align='left' p='5px'>
+          <Box fontWeight='bold' textTransform='uppercase'>{plant.common[1] ? plant.common[1] : plant.common[0]}</Box>
+          <Box fontWeight='bold' textTransform='uppercase'>Latin Name:</Box>
+          <Box pl='15px'>{plant.latin}</Box>
+          <Box fontWeight='bold' textTransform='uppercase'>Family:</Box>
+          <Box pl='15px'>{plant.family}</Box>
+          <Box fontWeight='bold' textTransform='uppercase'>Category:</Box>
+          <Box pl='15px'>{plant.category}</Box>
+          <Box fontWeight='bold' textTransform='uppercase'>Origin:</Box>
+          <Box pl='15px'>{plant.origin}</Box>
+          <Box fontWeight='bold' textTransform='uppercase'>Temperature Range:</Box>
+          <Box pl='15px'>{plant.tempmin.fahrenheit}-{plant.tempmax.fahrenheit} &#8457;</Box>
+          <Box fontWeight='bold' textTransform='uppercase'>Ideal Light:</Box>
+          <Box pl='15px'>{plant.ideallight}</Box>
+          <Box fontWeight='bold' textTransform='uppercase'>Watering Instructions:</Box>
+          <Box pl='15px'>{plant.watering}</Box>
+        </Box>
       </Box>
     </Box>
   )
